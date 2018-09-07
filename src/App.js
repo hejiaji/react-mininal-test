532206a4b5ec0aefc0e2b7584bf955315eeb78a2
Jeremy He <jeremy.he@safetyculture.io>
2018-09-07 11:39:01 +1000

    update README

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Hey What's up?
         </p>
       </div>
     );
